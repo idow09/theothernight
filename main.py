3ba236393c71c53bd87e823eb330f07d0eed75fc
Ido Weiss <idow09@gmail.com>
2019-11-13 00:38:19 +0200

    Added a roadmap draft

--- main.py
+++ main.py
@@ -158,3 +158,9 @@ if __name__ == "__main__":
     _roles = [WerewolfRole(), SeerRole(), VillagerRole(), MasonRole(), MasonRole()]
     game = OneNight(_players, _roles)
     game.run()
+
+# Roles should all inherit Role, in a flat hierarchy. With only one abstract method: play
+# Players should have ids, which will be used in voting and other referencing instead of name
+# The game end should be somehow dependent upon the current participating roles.
+# Input and output should be decoupled from the core BL.
+# Async streams should be used as message buses in both communication directions.
